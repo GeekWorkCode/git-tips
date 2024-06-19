6eedee1cfe72ce2b879cda4743ac688b4e991021
zs <asd486@live.com>
2024-06-19 16:11:13 +0800

    测试

--- GitTest/GitTest/ViewController.swift
+++ GitTest/GitTest/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("11111")
+        print("111112222")
         
     }
 
---
+++ GitTest/GitTest/ZSViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ZSViewController.swift
+//  GitTest
+//
+//  Created by zs on 2024/6/19.
+//
+
+import UIKit
+
+class ZSViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("333333")
+        
+    }
+
+
+}
